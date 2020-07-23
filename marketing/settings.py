ca3e9f77e96d0677ceb916ea79297ba23fb54a9e
arian <arian.mousakhan@gmail.com>
2020-07-23 16:53:17 +0430

    updated requirements

--- marketing/settings.py
+++ marketing/settings.py
@@ -1,5 +1,6 @@
 import os
 import environ
+import django-heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -113,3 +114,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'product'
 LOGIN_URL = 'login'
+
+# django_heroku.setting(locals())
